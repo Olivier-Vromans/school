90eedeac82bf450463e6e97524bf95e98211f7ad
Olivier Vromans <oliviervromans@MacBook-Olivier.domain_not_set.invalid>
2020-11-28 17:29:05 +0100

    error message on the add form

--- prg2/Week 2/create.php
+++ prg2/Week 2/create.php
@@ -7,11 +7,11 @@
     <body>
         <h2 class="thead"> Vull het form in om een album toe te voegen.</h2>
         <form>
-            Artist: <input type="text" name="artist" required><br>
-            Album:  <input type="text" name="Album"required><br>
-            Release date: <input type="number" min="0000" max="99999" name="releaseYear" required><br>
-            Tracks: <input type="text" name="tracks" required><br>
-            Genre: <input type="text" name="genre" required><br>
+            Artist: <input type="text" name="artist" required="" oninvalid="this.setCustomValidity('Type artist name')" oninput="setCustomValidity('')"><br>
+            Album:  <input type="text" name="Album"required oninvalid="this.setCustomValidity('Type album name')" oninput="setCustomValidity('')"><br>
+            Release date: <input type="number" min="0000" max="99999" name="releaseYear" required="" oninvalid="this.setCustomValidity('Type release year')" oninput="setCustomValidity('')"><br>
+            Tracks: <input type="text" name="tracks" required="" oninvalid="this.setCustomValidity('Type how many tracks are on the album')" oninput="setCustomValidity('')"><br>
+            Genre: <input type="text" name="genre" required=""  oninvalid="this.setCustomValidity('Type the genre')" oninput="setCustomValidity('')"><br>
             <input type="submit" value="Submit">
         </form>
     </body>
