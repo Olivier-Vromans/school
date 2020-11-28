80a8a9f1c5834b1707d527cbd76978ebd2079130
Olivier Vromans <oliviervromans@MacBook-Olivier.domain_not_set.invalid>
2020-11-28 15:34:20 +0100

    add webfonts and details

--- prg2/Week 2/details.php
+++ prg2/Week 2/details.php
@@ -2,10 +2,27 @@
 include 'music.php';
 $album_id = array ($_GET["album_id"]);
 // turn $_GET in a value that can be used to read the right data
-foreach ($_GET as $key => $value) {
-    echo $albums["artist"][$value] . "</br>";
-    echo $albums["albumName"][$value] . "</br>";
-    echo $albums["releaseYear"][$value] . "</br>";
-    echo $albums["tracks"][$value] . "</br>";
-    echo $albums["genre"][$value] . "</br>";
-}
+// foreach ($album_id as $key => $value) {
+//     echo $albums["artist"][$value] . "</br>";
+//     echo $albums["albumName"][$value] . "</br>";
+//     echo $albums["releaseYear"][$value] . "</br>";
+//     echo $albums["tracks"][$value] . "</br>";
+//     echo $albums["genre"][$value] . "</br>";
+// }
+?>
+<html>
+    <table>
+    <? foreach ($album_id as $key => $value) { ?>
+        <thead>
+            <th> <h2 class="h2"> <? echo $albums["artist"][$value], " - ", $albums["albumName"][$value]; ?> </h2> </th>
+        </thead>
+        <tbody>
+            <tr>
+                <td> <p>Het album <? echo $albums["albumName"][$value]; ?> van <? echo $albums["artist"][$value]; ?>
+                kwam uit in <? echo $albums["releaseYear"][$value]; ?>, het album bestaat uit 
+                <? echo $albums["tracks"][$value]; ?> die onder het genre <? echo $albums["genre"][$value]; ?> vallen. </p> </td>
+            <tr>
+        </tbody>
+    <? } ?>
+    </table>
+</html>
